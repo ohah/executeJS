27165c5f009855172e5830cf20f290d537af289b
ohah <bookyoon173@gmail.com>
2025-10-27 02:21:30 +0900

    refactor: update imports to use @ path alias
    - Replace relative imports with @ alias
    - Update editor-page.tsx, app/index.tsx, main.tsx
    - Improve import readability and maintainability

--- apps/executeJS/src/app/index.tsx
+++ apps/executeJS/src/app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Providers } from './providers';
-import { EditorPage } from '../pages/editor/EditorPage';
+import { EditorPage } from '../pages/editor';
 
 export const App: React.FC = () => {
   return (
