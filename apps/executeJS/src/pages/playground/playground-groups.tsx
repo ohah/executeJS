7d6017eb125ef474a9a1139ba998dbf822afac7e
Bori-github <qhflrnfl4324@gmail.com>
2025-10-28 15:02:26 +0900

    feat: 탭 추가 기능

--- apps/executeJS/src/pages/playground/playground-groups.tsx
+++ apps/executeJS/src/pages/playground/playground-groups.tsx
@@ -12,13 +12,25 @@ const initialTabs: Tab[] = [{ id: 'playground-1', title: 'Playground 1' }];
 export const PlaygroundGroups: React.FC = () => {
   const [tabs, setTabs] = useState<Tab[]>(initialTabs);
 
+  const handleAddTab = () => {
+    setTabs((prevTabs) => {
+      const newTabId = `playground-${tabs.length + 1}`;
+      const newTab: Tab = {
+        id: newTabId,
+        title: `Playground ${tabs.length + 1}`,
+      };
+
+      return [...prevTabs, newTab];
+    });
+  };
+
   return (
-    <div>
-      <div className="flex items-center border-b border-slate-800">
-        <div className="flex">
+    <div className="overflow-hidden w-screen h-screen">
+      <div className="overflow-x-auto flex items-center border-b border-slate-800">
+        <div className="flex shrink-0">
           {tabs.map(({ id, title }) => {
             return (
-              <div key={id} className="p-2 border-r border-slate-800">
+              <div key={id} className="shrink-0 p-2 border-r border-slate-800">
                 {/* TODO: 탭 최대 너비에 따른 제목 ellipsis 처리 */}
                 <span>{title}</span>
               </div>
@@ -26,7 +38,11 @@ export const PlaygroundGroups: React.FC = () => {
           })}
         </div>
 
-        <button type="button" onClick={() => {}} className="p-2 cursor-pointer">
+        <button
+          type="button"
+          onClick={handleAddTab}
+          className="shrink-0 p-2 cursor-pointer"
+        >
           <PlusIcon />
         </button>
       </div>
