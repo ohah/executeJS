084fa8499cdea1fa678ae73c9601809a687dda8a
Bori-github <qhflrnfl4324@gmail.com>
2025-10-28 14:33:37 +0900

    refactor: rename folder editor > playground

--- apps/executeJS/src/app/index.tsx
+++ apps/executeJS/src/app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Providers } from './providers';
-import { EditorPage } from '../pages/editor';
+import { EditorPage } from '../pages/playground';
 
 export const App: React.FC = () => {
   return (
