13828afab6e418404d59ac6d51b6d51763886685
Bori-github <qhflrnfl4324@gmail.com>
2025-10-28 15:21:11 +0900

    feat: 탭 닫기 기능

--- apps/executeJS/src/pages/playground/playground-groups.tsx
+++ apps/executeJS/src/pages/playground/playground-groups.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { PlaygroundPage } from './playground-page';
-import { PlusIcon } from '@radix-ui/react-icons';
+import { Cross2Icon, PlusIcon } from '@radix-ui/react-icons';
 
 interface Tab {
   id: string;
@@ -11,6 +11,16 @@ const initialTabs: Tab[] = [{ id: 'playground-1', title: 'Playground 1' }];
 
 export const PlaygroundGroups: React.FC = () => {
   const [tabs, setTabs] = useState<Tab[]>(initialTabs);
+
+  const handleCloseTab = (tabId: string) => {
+    setTabs((prevTabs) => {
+      if (prevTabs.length === 1) {
+        return prevTabs; // 최소 하나의 탭은 유지
+      }
+
+      return prevTabs.filter((tab) => tab.id !== tabId);
+    });
+  };
 
   const handleAddTab = () => {
     setTabs((prevTabs) => {
@@ -30,9 +40,27 @@ export const PlaygroundGroups: React.FC = () => {
         <div className="flex shrink-0">
           {tabs.map(({ id, title }) => {
             return (
-              <div key={id} className="shrink-0 p-2 border-r border-slate-800">
+              <div
+                key={id}
+                className="shrink-0 flex items-center p-2 border-r border-slate-800"
+              >
                 {/* TODO: 탭 최대 너비에 따른 제목 ellipsis 처리 */}
-                <span>{title}</span>
+                <button
+                  type="button"
+                  onClick={() => {
+                    // TODO: 탭 전환 로직 @bori
+                  }}
+                  className="pr-2"
+                >
+                  {title}
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleCloseTab(id)}
+                  className="p-1 rounded-sm hover:bg-[rgba(255,255,255,0.2)] transition-colors cursor-pointer"
+                >
+                  <Cross2Icon />
+                </button>
               </div>
             );
           })}
